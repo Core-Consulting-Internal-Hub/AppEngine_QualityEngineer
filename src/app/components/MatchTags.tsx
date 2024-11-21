332348fb3e9481759afe21d8a8d7cb645538d4c7
BernardHwang <laohwang0523@gmail.com>
2024-11-21 11:48:16 +0800

    Updated Validation Way

--- src/app/components/MatchTags.tsx
+++ src/app/components/MatchTags.tsx
@@ -3,9 +3,30 @@ export const MatchTags = (props) => {
     props.queryResult.data?.records.forEach(record => {
       if (record?.tags && Array.isArray(record?.tags)) {
         for (const tag of record.tags) {
-          if (tag && typeof tag === 'string' && props.row.some(transaction => tag.includes(transaction))) {
+          if (tag && typeof tag === 'string' && props.row.some(transaction => {
+            const tagValue = tag.split(":")
+            return tagValue[tagValue.length - 1] === transaction
+          })) {
             // Use id as the unique key in Map
             sets.set(record.id, { name: record.name, id: record.id });
+          }
+        }
+      }
+    });
+  return sets;
+}
+
+export const MatchTagsWithTags = (props) => {
+  const sets = new Map<any, { name: any, id: any, tags: any }>();
+    props.queryResult.data?.records.forEach(record => {
+      if (record?.tags && Array.isArray(record?.tags)) {
+        for (const tag of record.tags) {
+          if (tag && typeof tag === 'string' && props.row.some(transaction => {
+            const tagValue = tag.split(":")
+            return tagValue[tagValue.length - 1] === transaction
+          })) {
+            // Use id as the unique key in Map
+            sets.set(record.id, { name: record.name, id: record.id, tags: record.tags });
           }
         }
       }
